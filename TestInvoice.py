5ccfa16a72f783b1c84fe5dcd253f82d8066bf7f
Stephen <scarey8@uncc.edu>
2021-03-08 12:37:48 -0500

    Added tests for improper and proper inputs into inputNumber

--- TestInvoice.py
+++ TestInvoice.py
@@ -8,19 +8,34 @@ def products():
                 'Notebook': {'qnt': 5, 'unit_price': 7.5, 'discount': 10}}
     return products
 
+
 @pytest.fixture()
 def invoice():
     invoice = Invoice()
-    return  invoice
+    return invoice
+
 
 def test_CanCalculateTotalImpurePrice(products):
     invoice = Invoice()
     assert invoice.totalImpurePrice(products) == 75
 
+
 def test_CanCalculateTotalDiscount(invoice, products):
     invoice.totalDiscount(products)
     assert invoice.totalDiscount(products) == 5.62
 
+
 def test_CanCalculateTotalPurePrice(invoice, products):
     invoice.totalPurePrice(products)
     assert invoice.totalPurePrice(products) == 69.38
+
+def test_properInputs(invoice, monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: "3")
+    unit_price = invoice.inputNumber("Input 3: ")
+    assert unit_price == 3
+
+def test_improperInputs(invoice, monkeypatch):
+        inputs = iter(['n', '3'])
+        monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+        unit_price = invoice.inputNumber("Input a non-number: ")
+        assert unit_price == 3
